ba2e83853bc4f113149356704f3445be3d9f64e7
SaintRec <saintrecd@gmail.com>
2018-05-13 15:45:17 -0500

    Módulo con tablas relacionales
    Many2one

--- petro/models/petros.py
+++ petro/models/petros.py
@@ -1,19 +1,44 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
-class petro(models.Model):
+class Producto(models.Model):
     _name = 'petro.producto'
 
-    codbod = fields.Char('Codigo Bodega')
-    descinv = fields.Char('Descripcion SubInventario')
+    codbod = fields.Char('Código Bodega')
+    descinv = fields.Char('Descripción SubInventario')
     grupmat = fields.Char('Grupo Material')
-    codart = fields.Char('Codigo Articulo')
-    cantstock = fields.Float('Cantidad Inventario Stock')
+    codart = fields.Char('Código Articulo')
+    cantstock = fields.Integer('Cantidad Inventario Stock')
     umed = fields.Char('Unidad Medida')
-    descart = fields.Text('Descripcion Articulo')
+    name = fields.Text('Descripcion Articulo')
+    cnew = fields.Char('Código Nuevo')
+    desp = fields.Char('Descripción Español')
+    ding = fields.Char('Descripción Ingles')
+    img = fields.Binary('Imagen del producto')
+    subgrupo_id = fields.Many2one('petro.subgrupo', string='Subgrupo')
 
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+
+class Clase(models.Model):
+    _name = 'petro.clase'
+
+    codclase = fields.Char('Código Clase')
+    name = fields.Char('Nombre')
+    desclase = fields.Text('Descripción de la Clase')
+
+
+class Grupo(models.Model):
+    _name = 'petro.grupo'
+
+    codgrupo = fields.Char('Codigo Grupo')
+    name = fields.Char('Nombre')
+    clase_id = fields.Many2one('petro.clase', string='Clase')
+
+
+class Subgrupo(models.Model):
+    _name = 'petro.subgrupo'
+
+    codsubg = fields.Char('Código Subgrupo')
+    name = fields.Char('Clase')
+    grupo_id = fields.Many2one('petro.grupo', string='Grupo')
